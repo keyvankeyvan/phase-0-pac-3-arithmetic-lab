c3cd94ca269a5905c59f8044ce4eadb5a62e5086
Keyvan Alonso <keyvanalonso@gmail.com>
2022-01-30 21:12:49 -0500

    commit tree f798c7c181766a5dc80299c0f303bcb0329f52fd

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+    let sum = a + b;
+    return sum;
+}
+
+function subtract (a, b){
+    let difference = a - b;
+    return difference;
+}
+
+function multiply (a, b){
+    let mult = a * b;
+    return mult;
+}
+
+function divide (a, b){
+    let divi = a / b;
+    return divi;
+}
+
+function increment(n){
+    let incd = ++n;
+    return incd;
+}
+
+function decrement(n){
+    let decd = --n;
+    return decd;
+}
+
+function makeInt(n){
+    let parsd = parseInt(n,10);
+    return parsd;
+}
+
+function preserveDecimal(n){
+    let parsd = parseFloat(n)
+    return parsd;
+}
